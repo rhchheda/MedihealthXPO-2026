59df4f4c3247306aeb010974c62315652a9b7582
rhchheda <rakesh.chheda@kletech.ac.in>
2026-06-18 16:41:52 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// Paste the deployed GAS Web App URL here after deployment (Deploy > New deployment > Web app > Execute as Me > Anyone)
+// Example: 'https://script.google.com/macros/s/AKfycb.../exec'
+const API_URL = 'https://script.google.com/macros/s/AKfycbxGrtdRCfhFBUTQLfBwo1NJwAbF-TZVCaeJXJ5VIkwNbTf-Ee20kLivn46G-uoH00ic/exec';
